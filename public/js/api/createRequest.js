4cc24bb1836309699f6d83a6aab3065e6eecaf80
SAGURULEV <boxersergu7@yandex.ru>
2021-06-09 14:22:08 +0300

    zadacha22

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,41 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const login = options.data.email;
+    const password = options.data.password;
+    const urlOption = option.url;
+    const url = urlOption + "?mail=" + login + "&password=" + password;
+    const record = options.method;
+
+    if(record === GET) {
+        const xhr = new XMLHttpRequest;
+        xhr.open( 'GET', url);
+        xhr.send();
+     } else if(record === POST) {
+     const xhr = new XMLHttpRequest,
+     formData = new FormData;
+     formData.append( 'mail', login );
+     formData.append( 'password', password );
+     xhr.open( 'POST', urlOption );
+     xhr.send( formData );
+    } 
+    if(xhr.readyState === 4 && xhr.status === 200) {
+        let response = JSON.parse(xhr.responseText);
+        console.log(response);
+    }
+    
+    callback: ( err, response ) => {
+        
+        /*
+          при успешном выполнении err = null, response содержит данные ответа
+        */
+        console.log( err ); // null
+        console.log( response );
+    }
+
+
+
+
+
 
 };
